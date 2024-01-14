9589b2c18e398cb0e643b8e4d809324251774dc2
Lorenzofini <lorenzo.fini2@studio.unibo.it>
2024-01-14 22:09:09 +0100

    fix

--- app/src/main/java/com/example/playersquiz/MainActivity.kt
+++ app/src/main/java/com/example/playersquiz/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.playersquiz
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playersquiz.databinding.MainActivityBinding
